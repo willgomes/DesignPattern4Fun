113bd17be1afb0336936bca87766b49b3839fc45
Will Neto <will_lovinf@LIVE.COM>
2018-06-27 18:55:17 -0300

    Removing useless Interface

--- DP4F.StrategyPattern/Strategy/Interfaces/ICalculateItemTransport.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DP4F.StrategyPattern.Strategy.Interfaces
-{
-    public interface ICalculateItemTransport
-    {        
-        double CalculateItem(int quantity, double price);
-    }
-}
